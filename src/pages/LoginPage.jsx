2ebed1976a6e3207e8fa5381e7d41c4aabed0d04
Peter Woods <woodspc@gmail.com>
2024-08-29 11:30:53 +0100

    services updated

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import { useState, useContext } from "react";
 // import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
-import authService from "../services/auth.service";
+import authService from "../../services/auth.service";
 
 // const API_URL = "http://localhost:5005";
 
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 // import axios from "axios";
-import authService from "../services/auth.service";
+import authService from "../../services/auth.service";
 
 // const API_URL = "http://localhost:5005";
 

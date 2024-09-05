c52688c38d9b121e18ce16ff7ddb6334bdaf17ea
Peter Woods <woodspc@gmail.com>
2024-09-05 11:01:07 +0100

    another update

--- src/pages/SightingsPage.jsx
+++ src/pages/SightingsPage.jsx
@@ -63,8 +63,8 @@ export default function Sightings({ sightings, getAnimalsWithSightings }) {
                   <img
                     src={sighting.image}
                     alt="image of sighting"
-                    width="40%"
-                    height="40%"
+                    width="50%"
+                    height="50%"
                   />
                 )}
                 <li>
